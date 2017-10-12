4a2ec8cb5c067ef44d3b840f692f85950ec421f7
Jianbo He <hjbxyz@gmail.com>
2017-10-12 09:35:52 +0800

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-# -lopenblasp / -lopenblaso
-g++ *.cpp -o assoc -s -O2 -std=c++11 -static -lopenblas -lgfortran -lquadmath
+rm -rf assoc-$1
+mkdir assoc-$1
+
+if [ $1 == "lnx64" ]; then
+
+    g++ *.cpp -o assoc-$1/assoc -s -O2 -std=c++11 -static -lopenblasp -lgfortran -lquadmath -lpthread
+
+elif [ $1 == "win32" ]; then
+
+    i686-w64-mingw32-g++ *.cpp -o assoc-$1/assoc.exe -s -O2 -std=c++11 -static -L./mingw32/lib -lopenblas -lgfortran -lquadmath
+
+elif [ $1 == "win64" ]; then
+
+    x86_64-w64-mingw32-g++ *.cpp -o assoc-$1/assoc.exe -s -O2 -std=c++11 -static -L./mingw64/lib -lopenblas -lgfortran -lquadmath
+
+fi
