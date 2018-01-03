59742712edafa4156461b2edf0ef0053ea4009b0
Jianbo He <hjbxyz@gmail.com>
2018-01-03 10:17:20 +0800

    Updated build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ if [ $1 == "lnx64" ]; then
 
 elif [ $1 == "win32" ]; then
 
-    i686-w64-mingw32-g++ *.cpp -o assoc-$1/assoc.exe -s -O2 -std=c++11 -static -L./mingw32/lib -lopenblas -lgfortran -lquadmath
+    i686-w64-mingw32-g++ *.cpp -o assoc-$1/assoc.exe -s -O2 -std=c++11 -static -L../mingw32/lib -lopenblas -lgfortran -lquadmath
 
 elif [ $1 == "win64" ]; then
 
-    x86_64-w64-mingw32-g++ *.cpp -o assoc-$1/assoc.exe -s -O2 -std=c++11 -static -L./mingw64/lib -lopenblas -lgfortran -lquadmath
+    x86_64-w64-mingw32-g++ *.cpp -o assoc-$1/assoc.exe -s -O2 -std=c++11 -static -L../mingw64/lib -lopenblas -lgfortran -lquadmath
 
 fi
